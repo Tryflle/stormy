b424cc19f4de816f4de56b286627c6c1dcc44c8f
ncwo <>
2024-03-11 22:41:14 -0700

    add inventory click + move both configs same folder

--- src/main/java/dev/stormy/client/module/modules/combat/AutoClicker.java
+++ src/main/java/dev/stormy/client/module/modules/combat/AutoClicker.java
@@ -9,20 +9,28 @@ import dev.stormy.client.utils.asm.HookUtils;
 import dev.stormy.client.utils.math.TimerUtils;
 import dev.stormy.client.utils.player.PlayerUtils;
 import net.minecraft.block.BlockLiquid;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.weavemc.loader.api.event.*;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import net.minecraft.util.MovingObjectPosition;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 @SuppressWarnings("unused")
 public class AutoClicker extends Module {
-    public static TickSetting breakBlocks, hitSelect;
+    public static TickSetting breakBlocks, hitSelect, inventoryFill;
     public static SliderSetting leftCPS;
     public boolean shouldClick, breakHeld = false;
+    private final Method playerMouseInput;
     TimerUtils t = new TimerUtils();
     long lastClickTime = 0;
     int lmb = mc.gameSettings.keyBindAttack.getKeyCode();
@@ -36,6 +44,13 @@ public class AutoClicker extends Module {
         this.registerSetting(leftCPS = new SliderSetting("CPS", 10.0D, 1.0D, 20.0D, 1.0D));
         this.registerSetting(breakBlocks = new TickSetting("Break blocks", false));
         this.registerSetting(hitSelect = new TickSetting("Hit Select", false));
+        this.registerSetting(inventoryFill = new TickSetting("Inventory Fill", false));
+        try {
+            this.playerMouseInput = GuiScreen.class.getMethod("mouseClicked", Integer.TYPE, Integer.TYPE, Integer.TYPE);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        if (this.playerMouseInput != null) this.playerMouseInput.setAccessible(true);
     }
 
     public boolean breakBlock() {
@@ -71,6 +86,10 @@ public class AutoClicker extends Module {
     @SubscribeEvent
     public void onRender(RenderHandEvent e) {
         randomizer();
+        if (inventoryFill.isToggled() && ((mc.currentScreen != null) || !mc.inGameHasFocus)) {
+            doInvClick();
+            return;
+        }
         if (PlayerUtils.isPlayerInGame() && Mouse.isButtonDown(0) && shouldClick && mc.currentScreen == null) {
             if (hitSelect.isToggled() && !hitSelectLogic()) return;
             if (breakBlock()) return;
@@ -111,6 +130,33 @@ public class AutoClicker extends Module {
             HookUtils.setMouseButtonState(0, false);
             delaying = false;
             shouldClick = false;
+        }
+    }
+    public void doInvClick() {
+        if (((mc.currentScreen instanceof GuiInventory) || (mc.currentScreen instanceof GuiChest)) && Mouse.isButtonDown(0) && Keyboard.isKeyDown(54) && Keyboard.isKeyDown(42) && shouldClick) {
+            long currentTime = System.currentTimeMillis();
+            if (t.hasReached(Utils.Java.randomInt(5000, 10000))) {
+                delay = (1000 / (int) (leftCPS.getInput() + (Utils.Java.randomInt(-4, 0))));
+                t.reset();
+            } else delay = (1000 / (int) (leftCPS.getInput() + (Utils.Java.randomInt(-3, 3))));
+            if (delay < 0) delay = (1000 / (int) (leftCPS.getInput() + (Utils.Java.randomInt(0, 3))));
+            if (currentTime - lastClickTime >= delay && !delaying) {
+                lastClickTime = currentTime;
+                try {
+                    GuiScreen guiScreen = mc.currentScreen;
+                    this.playerMouseInput.invoke(guiScreen, (Mouse.getX() * guiScreen.width) / mc.displayWidth, guiScreen.height - ((Mouse.getY() * guiScreen.height) / mc.displayHeight) - 1, 0);
+                } catch (IllegalAccessException | InvocationTargetException ignored) {
+
+                }
+                delaying = true;
+            }
+            if (delaying) {
+                if (currentTime - lastClickTime >= Utils.Java.randomInt(30, 120)) {
+                    lastClickTime = currentTime;
+                    delaying = false;
+                    shouldClick = false;
+                }
+            }
         }
     }
 }
